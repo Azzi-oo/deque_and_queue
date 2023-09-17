454036d133481ef15f7fde0ef2c4e46c8d6ba184
Azat Yanberdin <azatdjon@yandex.ru>
2023-09-17 21:39:40 +0500

    first commit

---
+++ deque.py
@@ -0,0 +1,36 @@
+from collections import deque
+
+graph = {
+    "you": ["alice", "bob", "claire"],
+    "bob": ["anuj", "peggy"],
+    "alice": ["peggy"],
+    "claire": ["thom", "jonny"],
+    "anuj": [],
+    "peggy": [],
+    "thom": [],
+    "jonny": []
+}
+
+
+def person_is_seller(person):
+    # Your seller condition here
+    return person.endswith("m")
+
+
+def search(name):
+    search_queue = deque()
+    search_queue += graph[name]
+    searched = []
+    while search_queue:
+        person = search_queue.popleft()
+        if person not in searched:
+            if person_is_seller(person):
+                print(person + " is a mango seller!")
+                return True
+            else:
+                search_queue += graph[person]
+                searched.append(person)
+    return False
+
+
+search("you")
